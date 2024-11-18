6288f208ff4abfbe0884d37f80d7b640d7a94019
HoangHuy <hoanghuya1thptcc@gmail.com>
2024-11-18 07:24:06 +0700

    Add Blog

---
+++ WebWooden/Controllers/BlogController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebWooden.Models;
+
+namespace Harmic.Controllers
+{
+	public class BlogController : Controller
+	{
+		private readonly WoodContext _context;
+		public BlogController(WoodContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("/blog/{alias}-{id}.html")]
+
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.TbNews == null)
+			{
+				return NotFound();
+			}
+
+			var blog = await _context.TbNews.FirstOrDefaultAsync(m => m.NewId == id);
+			if (blog == null)
+			{
+				return NotFound();
+			}
+
+			//// Gán danh sách bình luận đúng kiểu vào ViewBag
+			//ViewBag.blogComment = await _context.TbBlogComments
+			//	.Where(c => c.BlogId == id)
+			//	.ToListAsync();
+
+			return View(blog);
+		}
+
+
+
+
+	}
+}
---
+++ WebWooden/ViewComponents/BlogViewComponents.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebWooden.Models;
+
+
+public class NewsViewComponent : ViewComponent
+{
+	private readonly WoodContext _context;
+
+	public NewsViewComponent(WoodContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		var items = _context.tbblog.Where(m => (bool)m.IsActive);
+
+		return await Task.FromResult<IViewComponentResult>
+			(View(items.OrderByDescending(m => m.NewId).ToList()));
+	}
+}
